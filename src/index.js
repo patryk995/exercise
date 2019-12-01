8d72a56bf20649a739cb4fb0b2e2062adaaa84d1
Patryk Bartoševič <patryk.bartosevic@mediapark.com>
2019-12-01 17:36:20 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const { Component } = React;
+const { render } = ReactDOM;
+// state data for 3 counters
+const data = [{ id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }];
+class Counter extends Component {
+    render() {
+        const { value } = this.props;
+        return (
+            <div className="counter">
+                <b>{value}</b>
+                <div className="counter-controls">
+                    <button className="button is-danger is-small">-</button>
+                    <button className="button is-success is-small">+</button>
+                </div>
+            </div>
+        );
+    }
+}
+class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    render() {
+        return (
+            <div>
+                {data.map(counter => (
+                    <Counter key={counter.id} value={counter.value} />
+                ))}
+            </div>
+        );
+    }
+}
+render(<App />, document.querySelector("#root"));
