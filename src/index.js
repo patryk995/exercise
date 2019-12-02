44bd387461b3e592d213161e0e5e08059e59c14b
Patryk Bartoševič <patryk.bartosevic@mediapark.com>
2019-12-02 22:15:14 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -2,12 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 
-const { Component } = React;
+const { Component, PureComponent } = React;
 const { render } = ReactDOM;
 // state data for 3 counters
-const data = [{ id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }];
+const data = [
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 }
+];
 
-class Counter extends Component {
+class Total extends PureComponent {
+    render() {
+        const { value } = this.props;
+        return <div>Total: {value}</div>;
+    }
+}
+
+class AltCounter extends PureComponent {
+    onIncrementClick = () => this.props.onChange(this.props.value + 1);
+    onDecrementClick = () => this.props.onChange(this.props.value - 1);
+
+    render() {
+        const { value } = this.props;
+
+        return (
+            <div className="counter alternative">
+                <b>{value}</b>
+                <div className="counter-controls">
+                    <button
+                        onClick={this.onDecrementClick}
+                        className="button is-danger is-small"
+                    >
+                        -
+                    </button>
+                    <button
+                        onClick={this.onIncrementClick}
+                        className="button is-success is-small"
+                    >
+                        +
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Counter extends PureComponent {
     onIncrementClick = () => this.props.onIncrement(1);
     onDecrementClick = () => this.props.onDecrement(1);
 
@@ -39,38 +80,77 @@ class App extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            data: [...data]
+            data: [...data],
+            total: data.reduce(
+                (accumulator, counter) => (accumulator += counter.value),
+                0
+            )
         };
     }
 
-    onIncrement(id, valueToAdd) {
+    onIncrement = (id, valToAdd) => {
         const counterIndex = this.state.data.findIndex(
             counter => counter.id === id
         );
         const clonedData = [...this.state.data];
-        clonedData[counterIndex].value += valueToAdd;
-        this.setState({ data: clonedData });
-    }
-    onDecrement(id, valueToSub) {
+        clonedData[counterIndex].value += valToAdd;
+        this.setState(prevState => ({
+            data: clonedData,
+            total: prevState.total + valToAdd
+        }));
+    };
+
+    onDecrement = (id, valToSub) => {
         const counterIndex = this.state.data.findIndex(
             counter => counter.id === id
         );
         const clonedData = [...this.state.data];
-        clonedData[counterIndex].value -= valueToSub;
-        this.setState({ data: clonedData });
-    }
+        clonedData[counterIndex].value -= valToSub;
+        this.setState(prevState => ({
+            data: clonedData,
+            total: prevState.total - valToSub
+        }));
+    };
+
+    onChange = (id, updatedVal) => {
+        const counterIndex = this.state.data.findIndex(
+            counter => counter.id === id
+        );
+        const clonedData = [...this.state.data];
+        const diff = updatedVal - clonedData[counterIndex].value;
+        clonedData[counterIndex].value = updatedVal;
+        this.setState(prevState => ({
+            data: clonedData,
+            total: prevState.total + diff
+        }));
+    };
 
     render() {
+        const { total } = this.state;
         return (
             <div>
-                {data.map(counter => (
-                    <Counter
-                        key={counter.id}
-                        value={counter.value}
-                        onIncrement={this.onIncrement.bind(this, counter.id)}
-                        onDecrement={this.onDecrement.bind(this, counter.id)}
-                    />
-                ))}
+                {data.map((counter, index) =>
+                    index !== data.length - 1 ? (
+                        <Counter
+                            key={counter.id}
+                            value={counter.value}
+                            onIncrement={val =>
+                                this.onIncrement(counter.id, val)
+                            }
+                            onDecrement={val =>
+                                this.onDecrement(counter.id, val)
+                            }
+                        />
+                    ) : (
+                        <AltCounter
+                            key={counter.id}
+                            value={counter.value}
+                            onChange={val => this.onChange(counter.id, val)}
+                        />
+                    )
+                )}
+
+                <Total value={total} />
             </div>
         );
     }
