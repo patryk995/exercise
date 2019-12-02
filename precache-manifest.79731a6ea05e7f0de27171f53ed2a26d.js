6a2ef6482bf19684f8ec48187dff5002603e3832
Patryk Bartoševič <patryk.bartosevic@mediapark.com>
2019-12-02 22:26:28 +0200

    Updates

---
+++ precache-manifest.79731a6ea05e7f0de27171f53ed2a26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41458a301e270494b1ed8ca3933b30e",
+    "url": "/exercise/index.html"
+  },
+  {
+    "revision": "8be004a373c180472889",
+    "url": "/exercise/static/css/main.7f6711fa.chunk.css"
+  },
+  {
+    "revision": "5d8385e6eb824e2f2a39",
+    "url": "/exercise/static/js/2.6166e1a8.chunk.js"
+  },
+  {
+    "revision": "8be004a373c180472889",
+    "url": "/exercise/static/js/main.d2061b34.chunk.js"
+  },
+  {
+    "revision": "09941d53a8e096d3f021",
+    "url": "/exercise/static/js/runtime-main.de0600c9.js"
+  }
+]);
